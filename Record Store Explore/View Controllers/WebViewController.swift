6bfea69394768b41128bb24e2854e91957253403
Timo den Hartog <timodenhartog@outlook.com>
2018-01-30 23:27:02 +0100

    cleaned code and configured the right storelinks

---
+++ Record Store Explore/View Controllers/WebViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WebViewController.swift
+//  Record Store Explore
+//
+//  Created by Timo den Hartog on 30-01-18.
+//  Copyright © 2018 Timo den Hartog. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
